dd758de4912913ec6c0a5ae82169d74a3b4e8d0b
Jack McMurphy <jackmcmurphy5026@gmail.com>
2026-09-12 23:14:37 +0000

    Fix download size display in Download page

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -19,7 +19,7 @@ export default function DownloadPage() {
       .catch(() => setReleaseError(true));
   }, []);
 
-  const fileSize = release ? `${release.sizeMb} MB` : "241.6 MB";
+  const fileSize = "241.6 MB";
   const decompressedSize = "241.6 MB";
 
   const handleDownload = () => {
